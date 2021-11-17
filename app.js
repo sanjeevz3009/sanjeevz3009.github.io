a1a21d63928300f14e7e6e85554ae40b4d79f867
UP2002235 <91495869+UP2002235@users.noreply.github.com>
2021-11-17 17:06:56 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,6 @@
+// Dark and light mode
+// Landing animation
+// Loading animation
+// Few animations in the website
+// Contact me page/ system
+// Home, about, portfolio, resume, maybe projects and contact me
