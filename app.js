940ec57a8258c2e6823101806f81661e70cb483a
UP2002235 <91495869+UP2002235@users.noreply.github.com>
2021-11-19 02:54:31 +0000

    Back to top SVG animation added.

--- app.js
+++ app.js
@@ -4,3 +4,19 @@
 // Few animations in the website
 // Contact me page/ system
 // Home, about, portfolio, resume, maybe projects and contact me
+
+function backToTop() {
+    const animation = document.querySelector('.animation');
+    const svg = document.getElementById('triangles');
+    svg.onclick = (e) => {
+        const colors = ['red', 'blue', 'green', 'orange', 'pink', 'purple']
+        const rando = () => colors[Math.floor(Math.random() * colors.length)];
+        document.documentElement.style.cssText = `
+        --dark-color: ${rando()};
+        --light-color: ${rando()};
+        `
+        window.scrollTo({top:0, left:0, behavior:"smooth"});
+    }
+}
+
+window.addEventListener('load', backToTop);
